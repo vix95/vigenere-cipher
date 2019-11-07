4563677486974c5b9a474feb936638ba9778c73b
vix95 <38139321+vix95@users.noreply.github.com>
2019-11-07 10:31:56 +0200

    working on break the key

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -13,6 +13,9 @@ public class Vigenere {
             String cmd;
             String key;
 
+            VignereCipher vignereCipher2 = new VignereCipher();
+            vignereCipher2.breakCipher(path);
+
             do {
                 cmd = vigenereMenu.readInput();
                 if (cmd != null && !cmd.equals("-q")) {
@@ -38,7 +41,9 @@ public class Vigenere {
                             }
 
                             break;
-                        case "-k":  // find key
+                        case "-k":  // break key
+                            VignereCipher vignereCipher = new VignereCipher();
+                            vignereCipher.breakCipher(path);
                             break;
                     }
                 }
--- src/VignereCipher.java
+++ src/VignereCipher.java
@@ -2,14 +2,20 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class VignereCipher {
+class VignereCipher {
     private int m = 26; // chars qty
     private char[] key;
+    private final static int num_trials = 40;
+    private ArrayList<String> keys = new ArrayList<>(); // keys, it's related with standard deviation
 
-    public VignereCipher() {
+    private final static int[] freq = { // frequency of every character; taken from wiki
+            82, 15, 28, 43, 127, 22, 20, 61, 70, 2, 8, 40, 24, 67, 75, 29, 1, 60, 63, 91, 28, 10, 23, 1, 20, 1
+    };
+
+    VignereCipher() {
     }
 
     VignereCipher(String key) {
@@ -29,7 +35,7 @@ public class VignereCipher {
         return encrypted_line;
     }
 
-    void encryptFile(String path) throws IOException {
+    void encryptFile(final String path) throws IOException {
         if (this.key != null) {
             BufferedWriter writer = new BufferedWriter(new FileWriter(path + "/crypto.txt"));
             Scanner scanner = new Scanner(new File(path + "/plain.txt"));
@@ -63,7 +69,7 @@ public class VignereCipher {
         return decrypted_line;
     }
 
-    void decryptFile(String path) throws IOException {
+    void decryptFile(final String path) throws IOException {
         if (this.key != null) {
             BufferedWriter writer = new BufferedWriter(new FileWriter(path + "/decrypt.txt"));
             Scanner scanner = new Scanner(new File(path + "/crypto.txt"));
@@ -84,50 +90,161 @@ public class VignereCipher {
         }
     }
 
-    void decryptFileFindKey(String path) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(path + "/decrypt.txt"));
-        BufferedWriter writerKey = new BufferedWriter(new FileWriter(path + "/key-new.txt"));
-        Scanner scanner = new Scanner(new File(path + "/crypto.txt"));
-        Scanner scannerPlain = new Scanner(new File(path + "/plain.txt"));
-        char[] template = scannerPlain.nextLine().toCharArray();
+    private boolean cryptanalysis(final char[] encrypted) {
+        // after collected all lines can break the cipher
+        double fit, best_fit = 1e100;
+        double[] stddev = new double[num_trials - 1]; // keep all standard deviation
+        int[] encrypted_int = new int[encrypted.length];
+        this.key = new char[encrypted.length];
 
-        boolean found_key = false;
-        while (scanner.hasNextLine()) {
-            char[] line = scanner.nextLine().toCharArray();
-            char[] decrypted_line = new char[0];
+        // write to array int codes for characters without spaces
+        int key_length = 0;
+        for (char c : encrypted) if (Character.isLetter(c)) encrypted_int[key_length++] = c - 'a';
 
-            // find the key
-            if (!found_key) {
-                for (int key = 1; key < 20; key++) {
-                    //this.key = ;
-                    //decrypted_line = this.decrypt(line);
+        // check configuration for j = 1, 2, 3, ...
+        for (int j = 1; j < num_trials; j++) {
+            fit = frequencyEveryPosition(encrypted_int, key_length, j, key); // fitness to key
+            stddev[j - 1] = fit;
 
-                    if (String.valueOf(decrypted_line).contains(String.valueOf(template))) {
-                        found_key = true;
-                        System.out.println("Found key: " + this.key);
-                        writerKey.write(this.key.toString());
-                        writerKey.close();
-                        writer.write(decrypted_line);
-                        writer.write('\n');
-                        break;
-                    }
+            // only for to see
+            StringBuilder builder = new StringBuilder();
+            for (char c : key) {
+                if (Character.isLetter(c)) {
+                    builder.append(c);
                 }
-            } else {
-                //decrypted_line = this.decrypt(line);
-                writer.write(decrypted_line);
-                writer.write('\n');
             }
 
-            if (found_key) {
-                System.out.println("encrypted line: " + String.valueOf(line));
-                System.out.println("decrypted line: " + String.valueOf(decrypted_line) + "\n");
-            } else {
-                System.out.println("Error: finding the key is impossible");
-                break;
+            keys.add(builder.toString());
+            System.out.printf("%f, key length: %2d - %s\n", fit, j, builder.toString());
+            if (fit < best_fit) best_fit = fit;
+        }
+
+        // looking for the correct key based on standard deviation
+        boolean found_key = false;
+        double min = stddev[0];
+        int index = 0;
+        for (int i = 0; i < stddev.length; i++) {
+            if (stddev[i] < min) {
+                min = stddev[i];
+                index = i;
+                found_key = true;
             }
         }
 
-        writer.close();
+        System.out.printf("The key is: '%s' for stddev = %f\n", keys.get(index), min);
+        this.key = keys.get(index).toCharArray();
+
+        return found_key;
+    }
+
+    void breakCipher(final String path) throws IOException {
+        Scanner scanner = new Scanner(new File(path + "/crypto.txt"));
+        char[] encrypted = new char[0];
+
+        while (scanner.hasNextLine()) {
+            char[] line = scanner.nextLine().toCharArray();
+            char[] temp = encrypted;
+            encrypted = new char[temp.length + line.length + 1]; // +1 for the space
+
+            // first copy already available encrypted text
+            int pos = 0;
+            for (char c : temp) {
+                encrypted[pos++] = c;
+            }
+
+            // next assign characters from line to collect every in one array
+            for (char c : line) {
+                encrypted[pos++] = c;
+            }
+
+            encrypted[pos] = ' '; // add space for clear view
+        }
+
         scanner.close();
+
+        if (this.cryptanalysis(encrypted)) {
+            System.out.println("encrypted line: " + String.valueOf(encrypted));
+            System.out.println("decrypted line: " + String.valueOf(encrypted) + "\n");
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path + "/decrypt.txt"));
+            BufferedWriter writerKey = new BufferedWriter(new FileWriter(path + "/key-crypto.txt"));
+
+            writer.close();
+            writerKey.close();
+        } else {
+            System.out.println("Error: finding the key is impossible");
+        }
+    }
+
+    private int checkMatch(final int[] freq_chars) {
+        double sum = 0;
+
+        // sum of every frequency of char
+        for (int i = 0; i < this.m; i++)
+            sum += freq_chars[i];
+
+        // looking for the best rotate
+        // the best rotate can describe chars of key
+        int best_offset = 0;
+        double best_fit = 0;
+        for (int offset = 0; offset < this.m; offset++) {
+            double fit = 0; //fit of every offset
+            for (int i = 0; i < this.m; i++) {
+                // it works based on Caesar, on every offset (k = 1, 2, 3, ...)
+                // is checking the fit by formula
+                // [frequency form chars by every offset] / [total of frequency of all chars] - [frequency char by char]
+                double v = freq_chars[(i + offset) % this.m] / sum - freq[i];
+                fit += v * v / freq[i]; // calculate the fit on scalar vectors
+            }
+
+            // looking for the best fit
+            if (best_fit == 0) {
+                best_fit = fit;
+                best_offset = offset;
+            } else if (fit < best_fit) {
+                best_fit = fit;
+                best_offset = offset;
+            }
+        }
+
+        return best_offset;
+    }
+
+    private double frequencyEveryPosition(final int[] encrypted, int len, int offset, char[] key) {
+        double d;
+        double[] accu = new double[this.m];
+
+        for (int j = 0; j < offset; j++) {
+            int[] freq_chars = new int[this.m]; // frequency of every char
+
+            // count of frequency of every char by offset
+            for (int i = j; i < len; i += offset)
+                freq_chars[encrypted[i]]++;
+
+            int match = checkMatch(freq_chars); // get the best match basing on vectors and fit
+
+            // write the letter for key
+            try {
+                key[j] = (char) (match + 'a');
+            } catch (Exception e) {
+                System.out.print(e.getMessage());
+            }
+
+            for (int i = 0; i < this.m; i++)
+                accu[i] += freq_chars[(i + match) % this.m];
+        }
+
+        double sum = 0;
+        for (int i = 0; i < this.m; i++)
+            sum += accu[i];
+
+        double ret = 0;
+        for (int i = 0; i < this.m; i++) {
+            d = accu[i] / sum - freq[i];
+            ret += d * d / freq[i];
+        }
+
+        key[offset] = '\0';
+        return ret;
     }
 }
